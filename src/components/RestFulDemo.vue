d9e8ed956126e77fb4a64599a6eee496caad2075
SIlevanlinh <levanlinhcube@gmail.com>
2018-06-10 16:29:13 +0900

    create RestFulDemo component

--- src/components/RestFulDemo.vue
+++ src/components/RestFulDemo.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="restful-demo">
-
+        <ul id="example-1">
+        <li v-for="person in people" :key="person.id">            
+            <img :src="person.avatar">
+            {{ person.name }}
+        </li>
+        </ul>
     </div>
 </template>
 
@@ -8,11 +13,24 @@
 export default {
     data () {
         return {
-            api: ''
+            people: [],
+            api: 'http://localhost:3000/people'
         }
     },
+    mounted () {
+        this.fetchOk(this.api)
+        .then(response =>
+            response.json()
+            .then(data => {
+                this.people = data
+            })
+        )
+        .catch(err => console.log(err))
+    },
     methods: {
-        
+        fetchOk: api =>
+            fetch(api)
+            .then(res => res.ok ? res : res.json().then(err => Promise.reject(err)))
     }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,21 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import FilterDemo from '@/components/FilterDemo.vue'
+import RestFulDemo from '@/components/RestFulDemo.vue'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
     routes: [
         {
-          path: '/',
-          name: 'home',
-          component: FilterDemo
+            path: '/',
+            name: 'home',
+            component: FilterDemo
+        },
+        {
+            path: '/restfuldemo',
+            name: 'restfuldemo',
+            component: RestFulDemo
         }
     ]
 })
